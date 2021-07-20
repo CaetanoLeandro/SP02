74e6a5e4b6843b67b35d912af98a02f607f199c2
Giovane <giovanefernandes35@gmail.com>
2021-07-20 19:55:13 -0300

    Updating example class method reference

--- src/br/com/apresentacoes/sprint02/lambda/methodReference/Main.java
+++ src/br/com/apresentacoes/sprint02/lambda/methodReference/Main.java
@@ -2,6 +2,7 @@ package br.com.apresentacoes.sprint02.lambda.methodReference;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
@@ -12,11 +13,21 @@ public class Main {
             System.out.println(nome);
         }
 
+
         // com uso da expressao lambda
         nomes.forEach(nome -> System.out.println(nome));
+
 
         // com uso do method reference
         nomes.forEach(System.out::println);
 
+
+        // sem a possibilidade de method reference
+        List<String> nomesReduzidos = nomes.stream()
+                .map(nome -> nome.substring(1))   // mostrar só a primeira letra dos nomes
+                .collect(Collectors.toList());
+
+        System.out.println(nomesReduzidos);
+        
     }
 }
