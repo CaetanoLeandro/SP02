eeb6023cb37d1fc614cfcf54527d53d3c6a1b0c5
Giovane <giovanefernandes35@gmail.com>
2021-07-20 12:47:55 -0300

    Updating pipeline

--- src/br/com/apresentacoes/sprint02/streams/pipeline/Pipeline.java
+++ src/br/com/apresentacoes/sprint02/streams/pipeline/Pipeline.java
@@ -5,10 +5,11 @@ import java.util.stream.Collectors;
 
 public class Pipeline {
     public static void main(String[] args) {
-        // Java 7
         // Pegar os tres primeiros nomes das pessoas com menos de 25 anos
         // Ordenar pelo nome
 
+
+        // Java 7
         List<Pessoa> pessoas = Pessoa.bancoDeDados();
         Collections.sort(pessoas, new Comparator<Pessoa>() {      // aceita lambda
             @Override
@@ -33,10 +34,7 @@ public class Pipeline {
         // Operacoes intermediarias
         // Operacoes finais
 
-
-        // Stream = fluxo de dados
-
-        List<String> nomes02 =
+        List<String> pessoas2 =
                 pessoas.stream()
                         //operacoes intermediarias
                         .filter(p -> p.getIdade() < 25)
@@ -46,25 +44,24 @@ public class Pipeline {
                         .map(Pessoa::getNome)
                         // operacoes finais
                         .collect(Collectors.toList()); // coleta os elementos e retorna uma lista
+        System.out.println(pessoas2);
 
 
-        System.out.println(nomes02);
+        // Outros metodos
+        List<Integer> lista = Arrays.asList(2,3,3,7,4,95,100,23,1);
 
+        lista.stream()
 
-      List<Integer> lista = Arrays.asList(2,3,3,7,4,95,100,23,1);
+                //operacoes intermediarias
+                .skip(4)    // pula os elementos de acordo com o parametro
+                .limit(5)   // limita o stream a "n" elementos // short-circuinting
+                .distinct() // nao permite que o stream processe elementos repetidos // utiliza equals e hashcode
+                .filter(e -> e % 2 == 0) // filtra os elementos baseado a uma condicao dada no parametro, representada em expressao lambda
+                .map(x -> x + 10)   // aplica uma funcao a cada elemento da stream // nesse exemplo, soma-se 10
+                .sorted(Comparator.naturalOrder()) // classifica os elementos utilizando comparador // nesse caso do elemento menor ao maior
 
-      lista.stream()
-               //operacoes intermediarias
-              .skip(4)    // pula os elementos de acordo com o parametro
-              .limit(5)   // limita o stream a "n" elementos // short-circuinting
-              .distinct() // nao permite que o stream processe elementos repetidos // utiliza equals e hashcode
-              .filter(e -> e % 2 == 0) // filtra os elementos baseado a uma condicao dada no parametro, representada em expressao lambda
-              .map(x -> x + 10)   // aplica uma funcao a cada elemento da stream // nesse exemplo, soma-se 10
-              .sorted(Comparator.naturalOrder()) // classifica os elementos utilizando comparador // nesse caso do elemento menor ao maior
-
-               // operacoes final
-              .forEach(System.out::println); // method reference
-               //.count();   // retorna a quantidade de elementos da stream
-
+                // operacoes final
+                .forEach(System.out::println); // method reference
+                //.count();   // retorna a quantidade de elementos da stream
     }
 }
