d80efe7655f7b709c68ebf328d1d41c55e90e358
Giovane <giovanefernandes35@gmail.com>
2021-07-29 11:50:33 -0300

    Add example optional

---
+++ src/br/com/apresentacoes/sprint02/apidatas/classes/Main.java
@@ -0,0 +1,23 @@
+package br.com.apresentacoes.sprint02.apidatas.classes;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+        LocalDate dataAtual = LocalDate.now();
+        System.out.println(dataAtual);
+
+    }
+}
+
+
+/*
+* CLASSES:
+*
+* LocalDate
+* LocalTime
+* LocalDateTime
+* Instant
+* ZonedDateTime
+*
+ */
---
+++ src/br/com/apresentacoes/sprint02/optional/Main.java
@@ -0,0 +1,43 @@
+package br.com.apresentacoes.sprint02.optional;
+
+import java.util.Optional;
+import java.util.stream.Stream;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String s = "Number one";
+        String n = "1";
+
+        Optional<Integer> num = convertNumber(n);
+        System.out.println(num.isPresent()); // retorna true ou false
+        System.out.println(num.get());       // pega o valor no Optional, mas se nao tiver valor lança exception
+
+
+
+        // outras maneiras de pegar valor no Optional ao inves de get()
+        Integer numero = convertNumber(s)
+                //.ifPresent(System.out::println);  // só executa lambda ou method reference se tiver valor
+                .orElse(2); // retorna o valor presente, senao retorna o valor do parametro
+                //.orElseGet(() -> {return operacaoPesada();});  // executa uma operacao mais pesada se valor nao for presente
+                //.orElseThrow(() -> new NullPointerException("Empty value")); // return value if present or throw exception if no value is present
+
+        System.out.println(numero);
+
+
+
+       Stream.of(1, 2, 3)
+                .findFirst()
+                .ifPresent(System.out::println);
+
+    }
+
+    public static Optional<Integer> convertNumber(String numberStr){
+            try {
+                Integer integer = Integer.valueOf(numberStr);
+                return Optional.of(integer);  // armazena valores
+            }catch (Exception e){
+                return Optional.empty();  // retorna uma instancia de Optional vazio
+            }
+    }
+}
