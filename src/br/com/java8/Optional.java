cf6d58c495a195ad7cc6de420a5c72e81da10775
Caetano <lcaetano30@gmail.com>
2021-11-05 09:25:48 -0300

    Transfers to Hub

---
+++ src/br/com/java8/Optional.java
@@ -0,0 +1,11 @@
+package br.com.java8;
+
+// Forma masi prática de trabalhar com números com valores null 
+public class Optional {
+
+    public static void main(String[] args) {
+
+    }
+
+    public  static Integer
+}
